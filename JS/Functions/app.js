cc008fff32a416883b6772e4009621bd0756a263
dilandurmaz <45289986+dilandurmaz@users.noreply.github.com>
2020-11-01 20:51:40 +0300

    functions

--- JS/Functions/app.js
+++ JS/Functions/app.js
@@ -19,3 +19,108 @@ sum(5,10);
 //-->15
 
 // Bindings and Scopes
+
+// If you create variables inside the function with let and const, they will not be seen outside the function.
+// But it is seen if it is created with var.
+const x = 10;
+if (true) {
+  let y = 20;
+  var z = 30;
+  console.log(x + y + z);
+  // → 60
+}
+// y is not visible here
+console.log(x + z);
+// → 40
+
+// Nested scope (İç içe)
+
+const sumPrice = function(sum){
+    const comp=function(amount,price){
+        sum = amount*price;
+        console.log(sum);
+        
+    };
+    comp(5,20);
+    comp(3,10);
+};
+sumPrice();
+// -->100
+// -->30
+
+// Functions as values
+let example = function(){
+    console.log("this is an examle");
+}
+if(true){
+    example = function(){
+        console.log("changed");
+    }
+}
+example();
+// --> changed
+
+
+// Declaration notation
+// Function declarations are not part of the regular top-to-bottom flow of control.
+console.log("The future says:", future());
+function future() {
+  return "You'll never have flying cars";
+}
+
+// Arrow functions
+
+// The arrow comes after the list of parameters and is followed by the function’s body. 
+// It expresses something like “this input (the parameters) produces this result (the body)”.
+
+const simple = (a,b)=>{
+    if(a<b){
+        console.log("The value of a is less than the value of b. ");
+    }
+    else if(a>b){
+        console.log("The value of b is less than the value of a. ");
+    }
+    else{
+        console.log("a is equal to b.");
+    }
+}
+simple(5,2);
+// 
+const square1 = (x) => { return x * x; };
+const square2 = x => x * x;
+// 
+const horn = () => {
+    console.log("Toot");
+};
+
+// Optional Arguments
+
+function square(x) { return x * x; }
+console.log(square(4, true, "hedgehog")); //If we give js too many parameters, it will ignore them.
+// → 16
+
+// If we give less parameters, it is defined as undefined.
+function minus(a, b) {
+    if (b === undefined) return -a;
+    else return a - b;
+}
+console.log(minus(10));
+// → -10
+console.log(minus(10, 5));
+// → 5
+
+// Another example
+function power(base, exponent = 2) {
+let result = 1;
+for (let count = 0; count < exponent; count++) {
+    result *= base;
+    }
+return result;
+}
+console.log(power(4));
+// → 16
+console.log(power(2, 6));
+// → 64
+
+
+// Closure
